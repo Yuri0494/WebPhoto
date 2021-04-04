a225b9e6c658455130b6f8130d1799fd989ee065
Yuri0494 <76704347+Yuri0494@users.noreply.github.com>
2021-04-04 20:09:53 +0300

    slider part4

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,30 +1,24 @@
 import React from "react";
 import "./slider.css";
 import { Transition } from 'react-transition-group';
-import photo1 from "./img(examples)/1.jpg";
-import photo2 from "./img(examples)/2.jpg";
-import photo3 from "./img(examples)/4.jpg";
-import photo4 from "./img(examples)/3.jpg";
 
 
 export default class Slider extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            photos: [
-                photo1,
-                photo2,
-                photo3,
-                photo4,
-            ],
+            photos: this.createAllPhotos(),
             sliderCounter: 0,
             active: false,
+            photoNowActive: false,
         }
         this.nextPhoto = this.nextPhoto.bind(this);
         this.backPhoto = this.backPhoto.bind(this);
         this.toggleActive = this.toggleActive.bind(this);
         this.setActiveTrue = this.setActiveTrue.bind(this);
         this.getThisSlide = this.getThisSlide.bind(this);
+        this.scalePhoto = this.scalePhoto.bind(this);
+        this.createAllPhotos = this.createAllPhotos.bind(this);
         }
 
 nextPhoto () {
@@ -59,8 +53,8 @@ toggleActive () {
 
 setActiveTrue () {
     return this.setState({
-        active: true
-    })
+            active: true
+        })
 }
 
 getThisSlide (eventTarget) {
@@ -73,13 +67,26 @@ getThisSlide (eventTarget) {
 
 }
 
+scalePhoto () {
+    return this.setState({
+        photoNowActive: !this.state.photoNowActive
+    })
+}
+
+createAllPhotos () {
+    let allPhotos = [];
+    for(let i = 0; i < 87; i++) {
+        allPhotos.push(`/img/${i+1}.jpg`);
+    }
+    return allPhotos;
+    }
+
     render () {
 
         // Фотографии из нижней ленты слайдера
         let photo1 = this.state.sliderCounter + 1;
         let photo2 = this.state.sliderCounter + 2;
         let photo3 = this.state.sliderCounter + 3;
-
 
         // Граничные значения для последовательной смены фотографий при переключении.
         if(photo1 > this.state.photos.length - 1) {
@@ -94,21 +101,41 @@ getThisSlide (eventTarget) {
             photo3-=this.state.photos.length;
         }
 
+        let styleForImg = null;
+        let styleForPodlojka = null;
+
+        if(this.state.photoNowActive) {
+            styleForImg = "slider__photo_now__img22";
+            styleForPodlojka = "podlojka__active";
+        } else {
+            styleForImg = "slider__photo_now__img";
+            styleForPodlojka = "podlojka";
+        }
+
         return (
             <div className="slider__wrapper">
+                <div className={styleForPodlojka}
+                    onClick={this.scalePhoto}
+                ></div>
                 <div className="slider__content">
                     <div className="slider__photo">
-                        <div className="arrow_container" onClick={() => {this.nextPhoto(), this.setActiveTrue()}}><div className="slider__arrow__next"></div></div>
-                            <Transition in={this.state.active} timeout={700} onEntering={this.toggleActive}>
+                        <div className="arrow_container" onClick={() => {this.nextPhoto(), this.setActiveTrue(), console.log()}}><div className="slider__arrow__next"></div></div>
+                            <Transition in={this.state.active} timeout={500} onEntered={this.toggleActive}>
                             {
                             state => 
                             (<div className={`slider__photo_now ${state}`}>
-                                <img src={this.state.photos[this.state.sliderCounter]}></img>
+                                <img className ={styleForImg} src={this.state.photos[this.state.sliderCounter]}
+                                onClick={(event) => {
+                                    this.scalePhoto();
+                                    console.log(event.target.src);
+                                }}
+                                ></img>
                             </div>)
                             }   
                             </Transition>
                         <div className="arrow_container" onClick={() => {this.backPhoto(), this.setActiveTrue()}}><div className="slider__arrow__back"></div></div>
                     </div>
+                    
                     <div className="slider__photos">
                     <Transition 
                         
@@ -142,7 +169,7 @@ getThisSlide (eventTarget) {
                     </Transition>
                     </div>
                 </div>
-            </div>
+        </div>
         )
     }
 }
